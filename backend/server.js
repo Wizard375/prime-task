668bbda32f7e119514ceb11bc073c6a8b054d53c
Wizard375 <suyashsha375@gmail.com>
2026-02-17 12:57:47 +0530

    new-update

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.json({ message: "API is running" });
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
